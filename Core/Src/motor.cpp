3310dbcb083da8502758bb3e0ccf09707d00bf5e
nuhHAIWCifuxqa <u202413925@hust.edu.cn>
2025-03-02 22:00:26 +0800

    updating...

--- Core/Src/motor.cpp
+++ Core/Src/motor.cpp
@@ -20,19 +20,19 @@ void motor_control(uint8_t name, uint8_t speed)
     {
         if (speed == 0)
         {
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, abs_max_speed);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, abs_max_speed);
         }
         else if (speed > 0)
         {
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, speed);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, 0);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, 0);
         }
         else if (speed < -0)
         {
             speed = -speed;
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, 0);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_1_Pin, 0);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, E_MOTOR_2_Pin, speed);
         }
 
     }
@@ -40,19 +40,19 @@ void motor_control(uint8_t name, uint8_t speed)
     {
         if (speed == 0)
         {
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, abs_max_speed);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, abs_max_speed);
         }
         else if (speed > 0)
         {
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, speed);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, 0);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, 0);
         }
         else if (speed < -0)
         {
             speed = -speed;
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, 0);
-            __HAL_TIM_SET_COUNTER(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, speed);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_1_Pin, 0);
+            __HAL_TIM_SetCompare(&E_S_MOTOR_TIM, S_MOTOR_2_Pin, speed);
         }
 
     }
@@ -60,19 +60,19 @@ void motor_control(uint8_t name, uint8_t speed)
     {
         if (speed == 0)
         {
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, abs_max_speed);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, abs_max_speed);
         }
         else if (speed > 0)
         {
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, speed);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, 0);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, 0);
         }
         else if (speed < -0)
         {
             speed = -speed;
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, 0);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_1_Pin, 0);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, W_MOTOR_2_Pin, speed);
         }
 
     }
@@ -80,19 +80,19 @@ void motor_control(uint8_t name, uint8_t speed)
     {
         if (speed == 0)
         {
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, abs_max_speed);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, abs_max_speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, abs_max_speed);
         }
         else if (speed > 0)
         {
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, speed);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, 0);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, 0);
         }
         else if (speed < -0)
         {
             speed = -speed;
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, 0);
-            __HAL_TIM_SET_COUNTER(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, speed);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_1_Pin, 0);
+            __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, speed);
         }
     }
 }
