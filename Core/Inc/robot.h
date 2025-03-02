12f314e21ce20b1bed62b6f5bf68a5deb747169c
nuhHAIWCifuxqa <u202413925@hust.edu.cn>
2025-03-02 21:24:34 +0800

    updating...

--- Core/Inc/robot.h
+++ Core/Inc/robot.h
@@ -4,21 +4,27 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+//规定车辆的N轮为参考正方向（x正），E为y正，逆时针旋转时角速度为正
+//但是目前还不知道麦轮是左旋还是右旋的，此代码中认为麦轮全部是右旋
 
+//定义一些车辆基本数据，1.23456表示待测值，参考提供的代码
 #define PID_rate 50
 
 #define R_of_robot 1.23456
 #define R_of_wheel 1.23456
 
-#define encoder_resolution 13
-#define encoder_gear_ratio 30
+#define encoder_resolution 13 //编码器分辨率
+#define encoder_gear_ratio 30 //减速比
 #define encoder_resolution_u (4 * encoder_resolution * encoder_gear_ratio)
 
 #define conversion_factor (2 * PI * R_of_wheel *PID_rate / \
-encoder_resolution_u)
+encoder_resolution_u) //轮子速度m/s与编码器转换系数
 
+#define  vertical_velocity_ratio 1 //麦轮运动时垂直方向的速度比，查阅资料得在 理想状态 下是1
+
+//可能需要的数学常数
 #define PI 3.14159265358979323846
-#define c 1.23456
+#define unknown 1.23456
 
 typedef struct
 {
