21267f32b22005be1cef2c6a20567b29f2f05d8b
nuhHAIWCifuxqa <u202413925@hust.edu.cn>
2025-03-02 21:14:38 +0800

    updating...

--- Core/Inc/robot.h
+++ Core/Inc/robot.h
@@ -5,6 +5,28 @@
 extern "C" {
 #endif
 
+#define PID_rate 50
+
+#define R_of_robot 1.23456
+#define R_of_wheel 1.23456
+
+#define encoder_resolution 13
+#define encoder_gear_ratio 30
+#define encoder_resolution_u (4 * encoder_resolution * encoder_gear_ratio)
+
+#define conversion_factor (2 * PI * R_of_wheel *PID_rate / \
+encoder_resolution_u)
+
+#define PI 3.14159265358979323846
+#define c 1.23456
+
+typedef struct
+{
+    double  recent_velocity;       //车轮实时速度，单位m/s
+    float  target_velocity;       //车轮目标速度，单位m/s
+    short  PWM_velocity;      //车轮PWM控制速度
+
+}robot_wheel;
 
 #ifdef __cplusplus
 }
