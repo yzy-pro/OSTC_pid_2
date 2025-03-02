d21a94d233fa9da88946d8932f6a4e1393af3d01
nuhHAIWCifuxqa <u202413925@hust.edu.cn>
2025-03-02 23:11:40 +0800

    updating...

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -7,14 +7,17 @@ extern "C" {
 #include <sys/_stdint.h>
 #define abs_max_speed 100
 
-void robot_go(float speed, uint8_t direction);
-//机器人整体控制函数
-//速度，方向（只支持四向移动，0,90,180,270）
-
 void motor_control(uint8_t name, uint8_t speed);
 //电机控制函数
 //轮子编号，速度（pwm）0~100
 
+void robot_go(float speed, uint8_t direction);
+//机器人整体直线运动控制函数
+//速度，方向（只支持四向移动，0,90,180,270）
+
+void robot_turn(float speed, uint8_t angle);
+//机器人整体旋转运动控制函数
+//速度，角度
 
 #ifdef __cplusplus
 }
--- Core/Src/motor.cpp
+++ Core/Src/motor.cpp
@@ -1,8 +1,9 @@
 #include "motor.h"
 #include "main.h"
 #include "tim.h"
+#include "robot.h"
 
-void motor_control(uint8_t name, uint8_t speed)
+void motor_control(const uint8_t name, uint8_t speed)
 {
     if (speed > abs_max_speed || speed < -abs_max_speed)
     {
@@ -95,4 +96,88 @@ void motor_control(uint8_t name, uint8_t speed)
             __HAL_TIM_SetCompare(&W_N_MOTOR_TIM, N_MOTOR_2_Pin, speed);
         }
     }
+}
+
+void robot_go(const float speed, const uint8_t direction)
+{
+    //应该是要根据实际电机和轮子改正负号
+    const int8_t pwm_speed = conversion_factor * speed;
+    const int8_t pwm_vertical_speed = vertical_velocity_ratio * pwm_speed;
+    if (speed == 0)
+    {
+        motor_control('E', 0);
+        motor_control('S', 0);
+        motor_control('W', 0);
+        motor_control('N', 0);
+        return;
+    }
+
+    if (direction == 0)
+    {
+        motor_control('E', pwm_vertical_speed);
+        motor_control('S', pwm_speed);
+        motor_control('W', -pwm_vertical_speed);
+        motor_control('N', -pwm_speed);
+    }
+    else if (direction == 90)
+    {
+        motor_control('S', pwm_vertical_speed);
+        motor_control('W', pwm_speed);
+        motor_control('N', -pwm_vertical_speed);
+        motor_control('E', -pwm_speed);
+    }
+    else if (direction == 180)
+    {
+        motor_control('W', pwm_vertical_speed);
+        motor_control('N', pwm_speed);
+        motor_control('E', -pwm_vertical_speed);
+        motor_control('S', -pwm_speed);
+    }
+    else if (direction == 270)
+    {
+        motor_control('N', pwm_vertical_speed);
+        motor_control('E', pwm_speed);
+        motor_control('S', -pwm_vertical_speed);
+        motor_control('W', -pwm_speed);
+    }
+
+}
+
+void robot_turn(float speed, uint8_t angle)
+{
+    //这个原地转圈肯定要优化，例如用中断取代Delay
+    const int8_t pwm_speed = conversion_factor * speed;
+    angle = angle % 360;
+
+    if (0 < angle || 0 == angle && angle < 180 || angle ==180)
+    {
+        const float rad_angle = angle * PI / 180;
+        const float turning_time = rad_angle * R_of_robot / speed * 1000;
+
+        motor_control('E', pwm_speed);
+        motor_control('S', pwm_speed);
+        motor_control('W', pwm_speed);
+        motor_control('N', pwm_speed);
+
+        HAL_Delay(turning_time);
+
+        robot_go(0, 0);
+    }
+    else
+    {
+        angle = 360 - angle;
+        const float rad_angle = angle * PI / 180;
+        const float turning_time = rad_angle * R_of_robot / speed * 1000;
+
+        motor_control('E', -pwm_speed);
+        motor_control('S', -pwm_speed);
+        motor_control('W', -pwm_speed);
+        motor_control('N', -pwm_speed);
+
+        HAL_Delay(turning_time);
+
+        robot_go(0, 0);
+    }
+
+
 }
